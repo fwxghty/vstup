4c728a0e7bde52848f845b987be46c278f2b8193
fwxghty <kirilsvirenko08@gmail.com>
2024-06-04 16:46:16 +0300

    rgr

---
+++ assets/js/global.js
@@ -0,0 +1,16 @@
+;
+(function( $ ) {
+    "use strict"
+
+    $(document).on('ready', function () { });
+
+}( jQuery ));
+
+document.addEventListener("DOMContentLoaded", (event) => {
+
+    Fancybox.bind('[data-fancybox]', { });
+
+});
+
+
+
